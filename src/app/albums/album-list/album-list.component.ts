1a5e4b3d5a504649b7f7e82fc2cb2ab9fe9c84c5
Michael Royski <mroyski@gmail.com>
2019-11-21 10:18:52 -0500

    album service

--- src/app/albums/album-list/album-list.component.ts
+++ src/app/albums/album-list/album-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { ALBUMS } from '../albums.data';
+import { AlbumService } from '../shared/album.service';
+
 @Component({
     selector: 'app-album-list',
     templateUrl: './album-list.component.html',
@@ -8,7 +10,10 @@ import { ALBUMS } from '../albums.data';
 })
 export class AlbumListComponent implements OnInit {
     albumsArray: Album[];
-    constructor() {}
+    getAlbums() {
+        this.albumsArray = this.albumService.getAlbums();
+    }
+    constructor(private albumService: AlbumService) {}
 
     ngOnInit(): void {
         this.albumsArray = this.albumsArray = ALBUMS;
---
+++ src/app/albums/shared/album.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { ALBUMS } from '../albums.data';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AlbumService {
+    constructor() {}
+    getAlbums() {
+        return ALBUMS;
+    }
+}
