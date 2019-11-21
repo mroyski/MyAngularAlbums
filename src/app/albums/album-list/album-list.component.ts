252947754ea95185978e94c7eaf2d81af316c792
Michael Royski <mroyski@gmail.com>
2019-11-21 13:56:55 -0500

    observable pipe

--- src/app/albums/album-list/album-list.component.ts
+++ src/app/albums/album-list/album-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { ALBUMS } from '../albums.data';
 import { AlbumService } from '../shared/album.service';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-album-list',
@@ -9,17 +10,14 @@ import { AlbumService } from '../shared/album.service';
     styleUrls: ['./album-list.component.css'],
 })
 export class AlbumListComponent implements OnInit {
-    albumsArray: Album[];
+    albumsArray: Observable<Album[]>;
     getAlbums() {
-        this.albumService.getAlbums().subscribe(
-            albums => (this.albumsArray = albums),
-            error => console.log('Error: ', error)
-        );
+        this.albumsArray = this.albumService.getAlbums();
     }
     constructor(private albumService: AlbumService) {}
 
     ngOnInit(): void {
-        this.albumsArray = this.albumsArray = ALBUMS;
+        this.getAlbums();
     }
     parentFunctionHandler(album) {
         alert(
